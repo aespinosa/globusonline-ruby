55ae5b5219d2bd17e3d69806e68238690e1ab7a4
Allan Espinosa <aespinosa@cs.uchicago.edu>
2012-04-02 19:25:52 -0500

    Removed hardwired paths

--- test/test_external_access.rb
+++ test/test_external_access.rb
@@ -1,26 +1,27 @@
 require 'test/unit'
+require 'json'
 
 class ExternalAccessTest < Test::Unit::TestCase
   def test_net_http_proxy
     x509_file = "/tmp/x509up_u#{Process.uid}"
-    skip
+    skip 'No proxy file found' if not File.exist?(x509_file)
     require "net/http"
     uri = URI.parse "https://transfer.api.globusonline.org/v0.10/tasksummary"
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    #cert_file = x509_file
-    #key_file = x509_file
-    cert_file = "/home/aespinosa/.globus/usercert.pem"
-    key_file = "/home/aespinosa/.globus/userkey.pem"
+    cert_file = x509_file
+    key_file = x509_file
+    #cert_file = "#{ENV['HOME']}/.globus/usercert.pem"
+    #key_file = "#{ENV['HOME']}/.globus/userkey.pem"
     http.cert = OpenSSL::X509::Certificate.new(File.read(cert_file))
     http.key = OpenSSL::PKey::RSA.new(File.read(key_file))
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.ca_file = "/home/aespinosa/globus/certificates/gd-bundle_ca.cert"
+    http.ca_file = "#{ENV['HOME']}/.globus/certificates/gd-bundle_ca.cert"
     req = Net::HTTP::Get.new(uri.request_uri)
-    req["X-Transfer-API-X509-User"] = "aespinosa"
+    req["X-Transfer-API-X509-User"] = ENV["USER"]
 
     res = http.request(req)
-    puts res.body
+    assert_equal "tasksummary", JSON.parse(res.body)['DATA_TYPE']
   end
 
   def test_curl
@@ -29,19 +30,19 @@ class ExternalAccessTest < Test::Unit::TestCase
     require 'curb'
     url =  "https://transfer.api.globusonline.org/v0.10/tasksummary"
     Curl::Easy.new(url) do |curl|
-      curl.headers["X-Transfer-API-X509-User"] = "aespinosa"
+      curl.headers["X-Transfer-API-X509-User"] = ENV["USER"]
       cert_file = x509_file
       key_file = x509_file
-      #cert_file = "/home/aespinosa/.globus/usercert.pem"
-      #key_file = "/home/aespinosa/.globus/userkey.pem"
-      curl.cacert = "/home/aespinosa/.globus/certificates/gd-bundle_ca.cert"
+      #cert_file = "#{ENV['HOME']}/.globus/usercert.pem"
+      #key_file = "#{ENV['HOME']}/.globus/userkey.pem"
+      curl.cacert = "#{ENV['HOME']}/.globus/certificates/gd-bundle_ca.cert"
       curl.cert = cert_file
       curl.cert_key = key_file
 
       curl.verbose = true
 
       curl.perform
-      puts curl.body_str
+      assert_equal "tasksummary", JSON.parse(res.body)['DATA_TYPE']
     end
   end
 end
